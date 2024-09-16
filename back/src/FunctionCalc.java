ac29709b495567c689d97684abe88beb1ae10988
2BuRy1 <kot514877@gmail.com>
2024-09-16 12:50:58 +0300

    fixed triangle function

--- back/src/FunctionCalc.java
+++ back/src/FunctionCalc.java
@@ -6,23 +6,16 @@ public class FunctionCalc {
 
     Logger logger = LoggerConfig.getLogger(this.getClass().getName());
     private boolean isTriangle(int x, double y, int r) {
-        if (x < 0 || y > 0) {
+
+        var equation = r/2*x - r/2;
+
+        if(y > 0 || x < 0 || y > equation){
+            logger.info("returned false with arguments: %s, %s, %s".formatted(x, y, r));
             return false;
         }
 
-        double xA = 0, yA = 0;
-        double xB = r, yB = 0;
-        double xC = 0, yC = -r / 2;
-
-        double S = 0.5 * Math.abs(xA * (yB - yC) + xB * (yC - yA) + xC * (yA - yB));
-
-        double S1 = 0.5 * Math.abs(x * (yA - yB) + xA * (yB - y) + xB * (y - yA));
-
-        double S2 = 0.5 * Math.abs(x * (yC - yA) + xA * (y - yC) + xC * (yA - y));
-
-        double S3 = 0.5 * Math.abs(x * (yB - yC) + xB * (yC - y) + xC * (y - yB));
-
-        return (S1 + S2 + S3) == S;
+        logger.info("returned true with arguments: %s, %s, %s".formatted(x, y, r));
+        return true;
     }
 
 
